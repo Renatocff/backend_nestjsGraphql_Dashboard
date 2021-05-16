112b135d45a4269fe58ada68d71d85c7380ea4e5
Renatocff <renatocff@gmail.com>
2021-05-16 11:19:45 -0300

    Melhorado update de Conta

--- src/conta/dto/update-conta.input.ts
+++ src/conta/dto/update-conta.input.ts
@@ -1,5 +1,5 @@
 import { InputType } from "@nestjs/graphql";
-import { IsEnum, IsInt, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength,  } from "class-validator";
+import { IsBoolean, IsEnum, IsInt, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength,  } from "class-validator";
 import { tipoConta } from "./tipo-conta";
 
 
@@ -21,6 +21,7 @@ export class UpdateContaInput {
     nrconta?: string;
 
     @IsEnum(tipoConta)
+    @IsOptional()
     @IsNotEmpty({message: 'Campo tipoconta não pode ser vazio.'})
     tipoconta?: tipoConta;
 
@@ -30,6 +31,11 @@ export class UpdateContaInput {
     @MaxLength(50, {message: 'O campo nrconta não pode ultrapassar 50 caracteres'})
     @IsNotEmpty({message: 'Campo banco não pode ser vazio.'})
     banco?: string;
+
+    @IsBoolean()
+    @IsOptional()
+    @IsNotEmpty({message: 'Campo ativo do tipo Boolean não pode ser vazio.'})
+    ativo?: boolean;
 
     @IsInt()
     @IsNotEmpty({message: 'Campo userId não pode ser vazio.'})
